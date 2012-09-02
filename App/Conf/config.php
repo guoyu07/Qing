515d8ca0bd806d0afd08235557914bc629c3e559
Viggo <master@ruidon.com>
2012-09-03 01:08:33 +0800

    配置数据库连接

--- App/Conf/config.php
+++ App/Conf/config.php
@@ -1,5 +1,11 @@
 <?php
 return array(
-	//'配置项'=>'配置值'
+	'DB_TYPE'               => 'mysql',     // 数据库类型
+    'DB_HOST'               => '127.0.0.1', // 服务器地址
+    'DB_NAME'               => 'ruidon_qing',          // 数据库名
+    'DB_USER'               => 'ruidon',      // 用户名
+    'DB_PWD'                => 'ruidon',          // 密码
+    'DB_PORT'               => '3306',        // 端口
+    'DB_PREFIX'             => 'v_',    // 数据库表前缀
 );
 ?>
